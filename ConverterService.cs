92617b16d9f6e3aec71e74ac067cdded1620946f
gap401 <48143728+gap401@users.noreply.github.com>
2021-03-20 21:38:40 -0400

    Add project files.

---
+++ ConverterService.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using Topshelf.Logging;
+
+namespace TestServiceKeepAwake
+{
+    public class ConverterService
+    {
+
+        private FileSystemWatcher _watcher;
+        private static readonly LogWriter _log = HostLogger.Get<ConverterService>();
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"c:\temp\a", "*_in.txt");
+
+            _watcher.Created += FileCreated;
+
+            _watcher.IncludeSubdirectories = false;
+
+            _watcher.EnableRaisingEvents = true;
+
+            _log.InfoFormat("Starting Service ... 'TestServiceKeepAwake.exe' ");
+
+            return true;
+
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            _log.InfoFormat("Starting conversion of '{0}'", e.FullPath);
+
+            string content = File.ReadAllText(e.FullPath);
+
+            string upperContent = content.ToUpperInvariant();
+
+            var dir = Path.GetDirectoryName(e.FullPath);
+
+            var convertedFileName = Path.GetFileName(e.FullPath) + ".converted";
+
+            var convertedPath = Path.Combine(dir, convertedFileName);
+
+            File.WriteAllText(convertedPath, upperContent);
+
+        }
+
+
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+
+            return true;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Topshelf;
+
+namespace TestServiceKeepAwake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HostFactory.Run(serviceConfig =>
+                            {
+
+                                serviceConfig.UseNLog();
+
+                                serviceConfig.Service<ConverterService>(serviceInstance =>
+                                        {
+                                            serviceInstance.ConstructUsing(
+                                                () => new ConverterService());
+
+
+                                            serviceInstance.WhenStarted(
+                                                execute => execute.Start());
+
+                                            serviceInstance.WhenStopped(
+                                                execute => execute.Stop());
+
+                                        });
+
+                                serviceConfig.SetServiceName("AwesomeFileConverter");
+                                serviceConfig.SetDisplayName("Awesome File Converter");
+                                serviceConfig.SetDescription("A Pluralsight demo service");
+
+                                serviceConfig.StartAutomatically();
+
+                            }
+
+            );
+
+
+        }
+    }
+}
